f56646c905e13d5a8b878e5432781a65e6340531
Jake Dec <jakedec@gmail.com>
2017-03-24 11:11:21 -0400

    Added main method

---
+++ main.cpp
@@ -0,0 +1,40 @@
+// Declarations and functions for project #4
+
+#include <iostream>
+#include <limits.h>
+#include "d_matrix.h"
+#include "d_except.h"
+#include <list>
+#include <fstream>
+
+int main()
+{
+	ifstream fin;
+
+	// Read the sample grid from the file.
+	string fileName = "sudoku.txt";
+
+	fin.open(fileName.c_str());
+	if (!fin)
+	{
+		cerr << "Cannot open " << fileName << endl;
+		exit(1);
+	}
+
+	try
+	{
+		board b1(SquareSize);
+
+		while (fin && fin.peek() != 'Z')
+		{
+			b1.initialize(fin);
+			b1.print();
+			b1.printConflicts();
+		}
+	}
+	catch (indexRangeError &ex)
+	{
+		cout << ex.what() << endl;
+		exit(1);
+	}
+}
--- p4.cpp
+++ p4.cpp
@@ -147,35 +147,3 @@ void board::print()
    cout << "-";
    cout << endl;
 }
-
-int main()
-{
-   ifstream fin;
-   
-   // Read the sample grid from the file.
-   string fileName = "sudoku.txt";
-
-   fin.open(fileName.c_str());
-   if (!fin)
-   {
-      cerr << "Cannot open " << fileName << endl;
-      exit(1);
-   }
-
-   try
-   {
-      board b1(SquareSize);
-
-      while (fin && fin.peek() != 'Z')
-      {
-	 b1.initialize(fin);
-	 b1.print();
-	 b1.printConflicts();
-      }
-   }
-   catch  (indexRangeError &ex)
-   {
-      cout << ex.what() << endl;
-      exit(1);
-   }
-}
