58895c3b0eca4d13846bb734d07d3407455ad446
Jake Dec <jakedec@gmail.com>
2017-04-05 14:30:45 -0400

    Updated main to read in multi sudoku puzzle files

--- main.cpp
+++ main.cpp
@@ -6,13 +6,18 @@
 #include "d_except.h"
 #include <list>
 #include <fstream>
+#include <vector>
 #include "board.h"
+
+vector<int> numMoves;
+
+void printStats();
 
 int main()
 {
     ifstream fin;
     // Read the sample grid from the file.
-    string fileName = "sudoku1.txt";
+    string fileName = "sudoku.txt";
 
     fin.open(fileName.c_str());
 
@@ -24,13 +29,16 @@ int main()
 
     try
     {
-        board b1(9);
-
         while (fin && fin.peek() != 'Z')
         {
+			cout << "Solving Board " << numMoves.size() + 1 << endl;
+			board b1(9);
             b1.initialize(fin); //Initialize board
 			int x = 0;
 			b1.solve(x); //start count at 0, must be passed by reference
+			numMoves.push_back(x);
+			cout << "Board " << numMoves.size() << " Solved in " << x << " moves!\n";
+			printStats();
         }//end loop over file
     }//end try statement
     catch (indexRangeError &ex)
@@ -42,3 +50,12 @@ int main()
     system("pause");
     return 0;
 }//end main function
+
+void printStats() {
+	int size = numMoves.size();
+	int totalMoves = 0;
+	for (int i = 0; i < size; i++) {
+		totalMoves += numMoves[i];
+	}
+	cout << "Average Solve Moves: " << (totalMoves / size) << endl;
+}
